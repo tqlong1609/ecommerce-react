b8299891750b8d8d038ef710a3f23dc5db390217
tqlong1609 <tqlong1609@gmail.com>
2021-10-03 17:16:05 +0700

    Added Product + Rating components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from "react";
+import Products from "./components/Products";
 import data from "./data";
-
-const MAX_RATING = 5;
-
-function displayStar(rating) {
-  if (Number.isInteger(rating) && rating <= MAX_RATING && rating >= 0) {
-    let arrStarSpan = ;
-    for (let i = 0; i < 5; i++) {
-      arrStarSpan += (
-        <span>
-          <i className="fa fa-star"></i>
-        </span>
-      );
-    }
-    console.log(arrStarSpan);
-    return arrStarSpan;
-  }
-  return null;
-}
 
 function App() {
   return (
@@ -35,35 +18,7 @@ function App() {
       </header>
       <main className="row center">
         {data.products.map((product) => (
-          <div key={product._id} className="card">
-            <a href="#">
-              <img className="medium" src={product.image} alt={product.name} />
-            </a>
-            <div className="card-body">
-              <a href="#">
-                <h2>{product.name}</h2>
-              </a>
-              <div className="rating">
-                {displayStar(product.rating)}
-                {/* <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span> */}
-              </div>
-              <div className="price">${product.price}</div>
-            </div>
-          </div>
+          <Products product={product} />
         ))}
       </main>
       <footer className="row center">All right reserved</footer>
