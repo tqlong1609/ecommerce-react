c6d5b7dc2c18f8d108b4306125c306a9da4d8a9e
tqlong1609 <tqlong1609@gmail.com>
2021-10-04 18:22:06 +0700

    Added ProductScreen theme + responsive

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,11 +5,11 @@ export default function Products(props) {
   const { product } = props;
   return (
     <div key={product._id} className="card">
-      <a href="#">
+      <a href={`/product/${product._id}`} >
         <img className="medium" src={product.image} alt={product.name} />
       </a>
       <div className="card-body">
-        <a href="#">
+        <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </a>
         <Rating rating={product.rating} numReviewer={product.numReviewer} />
